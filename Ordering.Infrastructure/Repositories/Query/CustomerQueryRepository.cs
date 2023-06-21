2cb93c4e5cd4357fcd1cb5dcd1c5b8bf40bd8ac5
AtharShihab <atharshihabc2@gmail.com>
2023-06-21 22:47:10 +0600

    AddedQueryRepos

---
+++ Ordering.Infrastructure/Repositories/Query/CustomerQueryRepository.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Extensions.Configuration;
+using Ordering.Core.Entities;
+using Ordering.Core.Repositories.Query;
+using Ordering.Infrastructure.Repositories.Query.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ordering.Infrastructure.Repositories.Query
+{
+    internal class CustomerQueryRepository : QueryRepository<Customer>,ICustomerQueryRepository
+    {
+        public CustomerQueryRepository(IConfiguration configuration) : base(configuration)
+        {
+        }
+
+        public Task<IReadOnlyList<Customer>> GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Customer> GetByIdAsync(long id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Customer> GetCustomerByEmailAsync(string email)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
