0cf7abf7340a745c7acb96dd8c67cbda3b064433
AtharShihab <atharshihabc2@gmail.com>
2023-06-21 22:04:58 +0600

    Initial commit

---
+++ Ordering.Core/Repositories/Query/ICustomerQueryRepository.cs
@@ -0,0 +1,13 @@
+﻿using Ordering.Core.Entities;
+using Ordering.Core.Repositories.Query.Base;
+
+namespace Ordering.Core.Repositories.Query
+{
+    public interface ICustomerQueryRepository : IQueryRepository<Customer>
+    {
+        // Custom operation which is not generic
+        Task<IReadOnlyList<Customer>> GetAllAsync();
+        Task<Customer> GetByIdAsync(Int64 id);
+        Task<Customer> GetCustomerByEmailAsync(string email);
+    }
+}
---
+++ Ordering.Infrastructure/Repositories/Command/CommandCustomerRepository.cs
@@ -0,0 +1,14 @@
+﻿using Ordering.Core.Entities;
+using Ordering.Core.Repositories.Command;
+using Ordering.Infrastructure.Data;
+using Ordering.Infrastructure.Repositories.Command.Base;
+
+namespace Ordering.Infrastructure.Repositories.Command
+{
+    public class CommandCustomerRepository : CommandRepository<Customer>, ICommandCustomerRepository
+    {
+        public CommandCustomerRepository(OrderingContext context) : base(context)
+        {
+        }
+    }
+}
